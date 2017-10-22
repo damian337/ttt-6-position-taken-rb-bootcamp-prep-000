822019cfa2391602569c017db01bf025f3c786e8
Damian Guillory <guillorywdamian@gmail.com>
2017-10-22 21:33:48 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,8 @@
 # code your #position_taken? method here!
 
 def position_taken?(array, index)
-    if array[index] == " " || ""
+    if array[index] == " " || "" || nil
         false
-    elsif array[index] == "X" || "O"
-        true
     else
-        nil
     end
 end
